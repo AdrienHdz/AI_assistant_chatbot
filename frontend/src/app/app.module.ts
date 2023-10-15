02ee547132392d457b7893b5d0a82733d78b2568
AdrienHdz <adrien.hernandez@hec.ca>
2023-10-15 19:42:21 -0400

    chore: Added FormsModule, ReactiveFormsModule and HttpClientModule in app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ChatMessagesComponent } from './chat-messages/chat-messages.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    ChatMessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
