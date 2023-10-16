84e2dbbee4b2dcf8b283c9221adf11dca18ba9dc
AdrienHdz <adrien.hernandez@hec.ca>
2023-10-15 21:51:52 -0400

    feat: Added redis cache to fastpi endpoint

--- backend/avatar_assistant_api/api/main.py
+++ backend/avatar_assistant_api/api/main.py
@@ -5,6 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 from api.generator.generator import get_openai_response, get_speech, get_wav2lip_url
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -29,9 +32,16 @@ class MessageRequest(BaseModel):
     message: str
 
 
+@app.on_event("startup")
+async def on_startup():
+    redis = RedisBackend("redis://redis:6379")
+    FastAPICache.init(backend=redis, prefix="fastapi-cache")
+
+
+@cache(expire=600)
 @app.post("/get_response/")
-async def get_message(request: MessageRequest):
-    processed_message = request.message.lower()
+async def get_message(message_request: MessageRequest):
+    processed_message = message_request.message.lower()
     openai_response = await get_openai_response(processed_message)
     await get_speech(openai_response)
     wav2lip_response = await get_wav2lip_url()
