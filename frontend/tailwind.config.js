970e88718cdfe29f0989bd362fa996300e86c2a3
AdrienHdz <adrien.hernandez@hec.ca>
2023-10-16 21:45:04 -0400

    feat: Added new font to frontend in tailwind config and message component and antialiazed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      'architects-daughter': ['"Architects Daughter"', 'sans-serif'],
+    },
+    fontSmoothing: {
+      antialiased: true
+    },
     extend: {},
   },
   plugins: [],
